571d32592cfa88a9d29470f28da577cd1f99191f
faisn <captainfaisalnazar@gmail.com>
2023-01-16 05:38:50 +0000

    Saved progress on MaxMinusMin 2023-01-16 05:38:50 +0000

--- MaxMinusMin/src/main/java/MaxMinusMin.java
+++ MaxMinusMin/src/main/java/MaxMinusMin.java
@@ -1,4 +1,5 @@
 
+import java.util.Collections;
 import java.util.List;
 
 public class MaxMinusMin {
@@ -10,6 +11,17 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        Collections.sort(nums);
+        int max = 0;
+        int min = 0;
+        int differenceBtwnMinAndMax = 0;
+        System.out.println(nums);
+        for (int i = 0; i < nums.size(); i++) {
+            max = nums.get(i);
+            min = nums.get(0);
+            differenceBtwnMinAndMax = max - min;
+        }
+        System.out.println(differenceBtwnMinAndMax);
+        return differenceBtwnMinAndMax;
     }
 }
