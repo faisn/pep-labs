8fe558b0231362f2839e034c4e6cd247315d8242
faisn <captainfaisalnazar@gmail.com>
2023-01-29 00:36:24 +0000

    Saved progress on ForeignKey 2023-01-29 00:36:24 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,1 +1,5 @@
-create table Post ( id serial primary key, post varchar(255) ,  artist_fk INT REFERENCES artist(id))
+create table Post( 
+    id serial primary key, 
+    post varchar(255), 
+    artist_fk int references site_user(id)
+);
