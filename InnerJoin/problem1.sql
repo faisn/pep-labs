1f9dfec517b3f38402fb3b62b854cc351d93db4d
faisn <captainfaisalnazar@gmail.com>
2023-02-05 04:43:34 +0000

    Saved progress on InnerJoin 2023-02-05 04:43:34 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,1 +1,1 @@
-select class.teacher_name, id, student_name from class inner join student on class.id= student.id where class.teacher_name = 'Ms. Lovelace';
+select class.teacher_name from class inner join student on class.class_title= student.student_id where class.teacher_name = 'Ms. Lovelace';
