fb51103d77cf39627057556e01429b8ed22fa948
faisn <captainfaisalnazar@gmail.com>
2023-02-14 01:25:36 +0000

    Saved progress on CharComparison 2023-02-14 01:25:36 +0000

--- CharComparison/src/main/java/CharComparison.java
+++ CharComparison/src/main/java/CharComparison.java
@@ -11,6 +11,18 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if(a[i] < b[j]) {
+                    return -1;
+                }else if (a[i] > b[j] && i == j) {
+                    return 1;
+                }else if(a[i] == b[j]) {
+                    return 0;
+                }
+            }
+        }
         return 0;
+        
     }
 }
